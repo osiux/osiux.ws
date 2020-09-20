b4386af097e0c4483336b60925119c7744d11b2b
Eduardo Reveles <me@osiux.ws>
2020-09-20 06:07:49 -0500

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import tw from 'twin.macro';
+import Img from 'react-optimized-image';
+
+import SEO from '@app/components/SEO';
+
+import Salem from '@images/salem.png';
+
+const Section = tw.section`flex flex-col content-center text-center pt-3`;
+
+const NotFoundPage = () => (
+    <>
+        <SEO title="404: Not found" />
+        <Section>
+            <Img
+                style={{ margin: '0 auto', clipPath: 'circle(50% at 50% 50%)' }}
+                src={Salem}
+                webp
+                sizes={[300]}
+            />
+            <h1>NOT FOUND</h1>
+            <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        </Section>
+    </>
+);
+
+export default NotFoundPage;
