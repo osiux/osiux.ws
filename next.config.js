e403ed09c1cc9230797d1f7e4608d403a7cfb1b2
Eduardo Reveles <me@osiux.ws>
2020-12-29 21:58:11 -0600

    try next offline

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')(['ky']);
 const optimizedImages = require('next-optimized-images');
+const offline = require('next-offline');
 
 const nextConfig = {
     images: {
-        domains: ['osiux.ws'],
+        domains: ['osiux.ws', 'www.osiux.ws'],
     },
     webpack: (config, { isServer }) => {
         // Fixes npm packages that depend on `fs` module
@@ -18,4 +19,40 @@ const nextConfig = {
     },
 };
 
-module.exports = withPlugins([withTM, optimizedImages], nextConfig);
+module.exports = withPlugins(
+    [
+        withTM,
+        optimizedImages,
+        [
+            offline,
+            {
+                workboxOpts: {
+                    swDest: process.env.NEXT_EXPORT
+                        ? 'service-worker.js'
+                        : 'static/service-worker.js',
+                    runtimeCaching: [
+                        {
+                            urlPattern: /^https?.*/,
+                            handler: 'NetworkFirst',
+                            options: {
+                                cacheName: 'offlineCache',
+                                expiration: {
+                                    maxEntries: 200,
+                                },
+                            },
+                        },
+                    ],
+                },
+                async rewrites() {
+                    return [
+                        {
+                            source: '/service-worker.js',
+                            destination: '/_next/static/service-worker.js',
+                        },
+                    ];
+                },
+            },
+        ],
+    ],
+    nextConfig,
+);
