7c7f3af6313988e407784d94b6962d8fdbdae7de
Eduardo Reveles <Eduardo Reveles>
2021-12-20 21:34:11 -0600

    Remove contact form

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import tw, { styled, css } from 'twin.macro';
+import tw, { styled } from 'twin.macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faGithub,
@@ -10,8 +9,11 @@ import {
 	faLastfm,
 } from '@fortawesome/free-brands-svg-icons';
 import { NextSeo } from 'next-seo';
+import Image from 'next/image';
 
 import Layout from '@app/components/Layout';
+
+import GetInTouch from '@images/get-in-touch.svg'
 
 const SocialNetworksList = tw.ul`list-none flex justify-center m-0 text-3xl`;
 
@@ -22,108 +24,23 @@ const SocialNetworkItem = styled.li`
 	}
 `;
 
-const Form = tw.form`flex w-full flex-col md:w-4/5 mx-auto`;
-const Field = tw.div`flex justify-end mb-4 flex-col md:flex-row md:flex-wrap`;
-const Label = tw.label`w-full mb-2 md:w-1/4 md:mt-1`;
-
-const baseInput = css`
-	${tw`w-full p-2 border border-gray-800 border-solid transition-colors duration-300 md:w-3/4 dark:(text-gray-900 bg-gray-300)`}
-`;
-
-const Input = styled.input`
-	${baseInput}
-`;
-
-const TextArea = styled.textarea`
-	${baseInput}
-`;
-
-const Button = styled.button`
-	${tw`transition-colors duration-300 border border-solid border-gray-800 bg-gray-900 text-gray-100 outline-none py-3 px-5 w-full self-end md:w-auto md:py-3 md:px-10 dark:(border-gray-100 bg-gray-600)`}
-
-	&[disabled] {
-		${tw`bg-gray-600 opacity-50 border-0`}
-	}
-`;
-
-const baseMessage = css`
-	${tw`w-full text-center mx-auto mb-2 p-2 border border-dotted md:w-4/5`}
-`;
-
-const Success = styled.p`
-	${baseMessage}
-	color: #4f8a10;
-	background: #dff2bf;
-	border: 1px solid #86bf27;
-`;
-
-const Error = styled.p`
-	${baseMessage}
-	color: #d8000c;
-	background: #ffd2d2;
-	border: 1px solid #800007;
-`;
-
 const Section = styled.section`
-	${tw`prose transition-colors duration-300 md:prose-xl max-w-full! dark:text-gray-100`}
+	${tw`prose md:prose-xl flex transition-colors duration-300 max-w-full! dark:text-gray-100`}
 
 	a {
 		${tw`transition-colors duration-300 dark:text-gray-100`}
 	}
 `;
 
-const Contact = () => {
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-	const [url, setUrl] = useState('');
-	const [message, setMessage] = useState('');
-	const [isSubmitting, setIsSubmitting] = useState(false);
-	const [error, setError] = useState<boolean | string>(false);
-	const [success, setSuccess] = useState(false);
-
-	const _onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-
-		setIsSubmitting(true);
-
-		try {
-			const response = await fetch('/api/contact', {
-				method: 'post',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ name, email, url, message }),
-			});
-
-			if (!response.ok) {
-				setError(
-					'Error sending the form, please try again or send me an email.',
-				);
-			} else {
-				setSuccess(true);
-				setName('');
-				setEmail('');
-				setUrl('');
-				setMessage('');
-			}
-		} catch (e) {
-			setError(
-				'Error sending the form, please try again or send me an email.',
-			);
-		}
-
-		setIsSubmitting(false);
-	};
-
-	return (
-		<Layout>
-			<Section>
-				<NextSeo title="Contact" />
+const Contact = () => (
+	<Layout>
+		<Section>
+			<NextSeo title="Contact" />
+			<div tw="flex items-center flex-col">
 				<p>
 					If you want to get in touch with me feel free to email me at{' '}
-					<a href="me@osiux.ws?subject=Hello!">me@osiux.ws</a>, send
-					me a message in any of my social networks, or you can use
-					the contact form below.
+					<a href="me@osiux.ws?subject=Hello!">me@osiux.ws</a> or send
+					me a message in any of my social networks.
 				</p>
 				<SocialNetworksList>
 					<SocialNetworkItem>
@@ -172,70 +89,13 @@ const Contact = () => {
 						</a>
 					</SocialNetworkItem>
 				</SocialNetworksList>
-
-				{success && (
-					<Success>Thanks! I&apos;ll be in touch shortly!</Success>
-				)}
-				{error && <Error>{error}</Error>}
-
-				<Form name="Contact" onSubmit={_onSubmit}>
-					<input type="hidden" name="form-name" value="Contact" />
-					<Field>
-						<Label htmlFor="name">Name:</Label>
-						<Input
-							type="text"
-							name="name"
-							id="name"
-							value={name}
-							onChange={(e) => setName(e.target.value)}
-							required
-						/>
-					</Field>
-
-					<Field>
-						<Label htmlFor="email">Email:</Label>
-						<Input
-							type="email"
-							name="email"
-							id="email"
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-							required
-						/>
-					</Field>
-
-					<Field tw="hidden">
-						<Label htmlFor="url">Url:</Label>
-						<Input
-							type="url"
-							name="url"
-							id="url"
-							value={url}
-							onChange={(e) => setUrl(e.target.value)}
-						/>
-					</Field>
-
-					<Field>
-						<Label htmlFor="message">Message:</Label>
-						<TextArea
-							name="message"
-							id="message"
-							rows={10}
-							onChange={(e) => setMessage(e.target.value)}
-							value={message}
-							required
-						/>
-					</Field>
-
-					<Field>
-						<Button type="submit" disabled={isSubmitting}>
-							{isSubmitting ? 'Sending...' : 'Send'}
-						</Button>
-					</Field>
-				</Form>
-			</Section>
-		</Layout>
-	);
-};
+			</div>
+			<div tw="text-center">
+				<Image src={GetInTouch} alt="Get in touch" />
+				<a tw="text-xs" href="https://storyset.com/online">Online illustrations by Storyset</a>
+			</div>
+		</Section>
+	</Layout>
+);
 
 export default Contact;
